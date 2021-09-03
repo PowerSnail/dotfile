49d80742bcdca720db4839a82d7cc602ec5e45d0
PowerSnail <hj@powersnail.com>
2021-09-03 00:23:03 -0700

    update fish conf

--- .config/fish/conf.d/pyenv.fish
+++ .config/fish/conf.d/pyenv.fish
@@ -1,3 +1,5 @@
 if type -q pyenv
   status --is-interactive; and source (pyenv init -|psub) 
+  pyenv init - | source
+  fish_add_path $HOME/.pyenv/shims
 end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,3 @@
-set -U fish_user_paths /home/henry/.cargo/bin $fish_user_paths
-set -U fish_user_paths /home/henry/.local/bin $fish_user_paths
-set -U LD_LIBRARY_PATH "/home/henry/.local/lib:/home/henry/.local/lib64"
-
-if type -q nvim
-  set -U EDITOR nvim
-end
-
 set -U SHELL fish
 
 abbr zdup "sudo zypper dup -l --no-recommends"
